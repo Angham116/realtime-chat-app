2e45cd80efaf38d9cf625d357efce519e8f0871a
Angham116 <angham.1161995@hotmail.com>
2020-01-24 15:27:38 +0200

    handling send msg socket emmiter

--- client/src/containers/Chat/index.jsx
+++ client/src/containers/Chat/index.jsx
@@ -2,34 +2,67 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import { Input, Button } from 'antd';
+
 let socket;
 const END_POINT = 'localhost:5000';
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState('');
+
+  // useEffect when user join the chat room
   useEffect(() => {
     const data = queryString.parse(location.search);
     socket = io(END_POINT);
-    // console.log(666, data)
     const { name, room } = data;
-    // console.log(name)
-    // console.log(room)
     setName(name);
     setRoom(room)
-    // console.log(555, socket)
     socket.emit('join-chat', { name, room }, () => {
     });
     return () => {
       socket.emit('disconnect');
       socket.off();
     }
-  }, [END_POINT, location.search])
-  // [END_POINT, location.search] where is the effect happen
-  // when rs for the server http or change the search contents
+    // [END_POINT, location.search] where is the effect happen
+    // when rs for the server http or change the search contents
+  }, [END_POINT, location.search]);
+
+  // useEffect for handling messages
+  useEffect(() => {
+    socket.on('join-chat-msg', (message) => {
+      setMessages([...messages, message]);
+    })
+  }, [messages]);
+
+  /*
+  * @description sendMessage is a service to send msg's
+  */
+  const sendMessage = e => {
+    e.preventDefault();
+    // console.log(222, e)
+    // console.log(333, message)
+    if(message){
+      socket.emit('send-msg', message, () => {
+        setMessage('');
+        // console.log(777, message)
+      })
+    }
+  }
+  console.log(888, message);
+  console.log(999, messages);
   return (
     <div>
-      Chat
+      <form onSubmit={e => sendMessage(e)}>
+        <Input
+          value={message}
+          onChange={({ target: { value } }) => setMessage(value)}
+          // onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+        />
+        <Button onClick={e => sendMessage(e)}>send</Button>
+      </form>
     </div>
   )
 }
